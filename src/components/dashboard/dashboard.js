17db9870f8837f1cd6823231f6dd5507fb37d8ec
Lauren <lauren.finley1@yahoo.com>
2020-05-12 14:59:19 -0500

    add dashboard component

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './dashboard.css';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="container">
+                Testing 11234345;
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
