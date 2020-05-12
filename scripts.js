261283067a47218f4bd712ed4ac0bb9d48c5a779
Lauren <lauren.finley1@yahoo.com>
2020-05-12 18:36:05 -0500

    finish styles for card area

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
-let card  = document.querySelector("#card");
+let cards = [
+    {
+        front: "card1 asd;fl asdflk aef;lewj f;oiweafj oweaijfkdj fa;si jfwea;oij fawe;oij f",
+        back: "back1"
+    },
+    {
+        front: "card2",
+        back: "back2"
+    },
+    {
+        front: "card3",
+        back: "back3"
+    }
+]
+
+let currentCard = 0;
 let showFront = true;
+
+let card  = document.querySelector("#card");
+
 function flipCard(){
     showFront = !showFront;
     showCard();
 }
 
 function showCard(){
-    let text = showFront ? "front" : "back";
+    let text = showFront ? cards[currentCard].front : cards[currentCard].back;
     card.innerText = text;
 }
 
-function generateDeckList() {
-    
+function getNextCard(){
+    currentCard < cards.length - 1 ? currentCard++ : currentCard = 0;
+    console.log(currentCard);
+    showFront = true;
+    showCard();
 }
 
 showCard();
