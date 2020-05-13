9e3e367b6d0eb51a2c19d58f346457bae825f1cb
Lauren <lauren.finley1@yahoo.com>
2020-05-12 19:18:13 -0500

    get flashcards from aws using api gateway api

--- scripts.js
+++ scripts.js
@@ -1,22 +1,17 @@
-let cards = [
-    {
-        front: "card1 asd;fl asdflk aef;lewj f;oiweafj oweaijfkdj fa;si jfwea;oij fawe;oij f",
-        back: "back1"
-    },
-    {
-        front: "card2",
-        back: "back2"
-    },
-    {
-        front: "card3",
-        back: "back3"
-    }
-]
+let cards;
 
 let currentCard = 0;
 let showFront = true;
 
 let card  = document.querySelector("#card");
+
+async function getCards(){
+    let response = await fetch("https://xsui7y6qu7.execute-api.us-east-1.amazonaws.com/prod/flashcards");
+    cards = await response.json();
+    cards = JSON.parse(cards.body);
+
+    showCard();
+}
 
 function flipCard(){
     showFront = !showFront;
@@ -30,9 +25,9 @@ function showCard(){
 
 function getNextCard(){
     currentCard < cards.length - 1 ? currentCard++ : currentCard = 0;
-    console.log(currentCard);
     showFront = true;
     showCard();
+    //console.log(new Date().getTime())
 }
 
-showCard();
+getCards();
